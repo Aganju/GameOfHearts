b7c1b9928849af20babb81c18c3a16c7816d689d
Omayemi Agbeyegebe <grend500@gmail.com>
2017-08-22 11:58:02 -0400

    add users jbuilders and bug fix

--- config/routes.rb
+++ config/routes.rb
@@ -54,8 +54,8 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
   root to: 'static_pages#root'
-  
-  namespace :api, defaults: { format: json } do
+
+  namespace :api, defaults: { format: :json } do
     resources :users, only: [:create]
     resource :session, only: [:create, :destroy]
   end
