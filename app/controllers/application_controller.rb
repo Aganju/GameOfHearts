a3a1ccca831b60a3bda8e736e97a92e2dc3bb74c
Omayemi Agbeyegebe <grend500@gmail.com>
2017-08-22 11:45:40 -0400

    create user signup, login and logout controller functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_methods :logged_in?, :current_user
+
+  def login(user)
+    session[:session_token] = user.session_token
+  end
+
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def logout
+    session[:session_token] = nil
+    current_user.reset_session_token
+  end
 end
