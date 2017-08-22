78d218c516a2729a186527cfa14a702c4d2fdadf
Omayemi Agbeyegebe <grend500@gmail.com>
2017-08-22 10:40:36 -0400

    create user table and model

---
+++ app/models/user.rb
@@ -0,0 +1,37 @@
+class User < ActiveRecord::Base
+  validates :username, :email, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  after_initialize :ensure_session_token
+  attr_reader :password
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+  def reset_session_token
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_toke
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return user if user && user.is_password?(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+end
