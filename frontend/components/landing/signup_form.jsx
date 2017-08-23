0035903334f258251c777ad0acf4e42b5ad27637
Omayemi Agbeyegebe <grend500@gmail.com>
2017-08-23 14:27:52 -0400

    create a sign up form component

---
+++ frontend/components/landing/signup_form.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { merge } from 'lodash';
+import FormSlice from './signup_form_slice';
+
+export default class SignupForm extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      username: '',
+      email: '',
+      password: '',
+      orientation: 'Straight',
+      gender: 'Female',
+      year: 0,
+      month: 0,
+      day: 0,
+      location: '',
+      formSlice: 0,
+    };
+
+    this.buttonTexts = ['Continue!', 'Next', 'Done!'];
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(inputName){
+    return (e) => {
+        this.setState({ [inputName]: e.currentTarget.value});
+    };
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+    if(this.state.formSlice === 2){
+      this.props.signUp(this.state).then((res)=>{console.log(res);});
+    }
+    else {
+      this.setState(merge({formSlice: this.state.formSlice++}, this.state));
+    }
+  }
+
+  render(){
+    const buttonText = this.buttonTexts[this.state.formSlice];
+
+    return (
+      <form>
+        <FormSlice
+          slice={this.state.formSlice}
+          handleChange={this.handleChange}
+          currentState={this.state}
+          />
+        <button onClick={ this.handleSubmit }>{ buttonText }</button>
+      </form>
+    );
+
+  }
+}
