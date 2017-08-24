05032fc4cada8032df53c48a2f2b50b4b1a85ea9
Omayemi Agbeyegebe <grend500@gmail.com>
2017-08-24 12:24:48 -0400

    create error handling actions

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -1,9 +1,16 @@
 import * as SessionUtil from '../utils/session_util';
 import { receiveCurrentUser } from './user_actions';
 
+export const RECEIVE_LOGIN_ERRORS = 'RECEIVE_LOGIN_ERRORS';
+
+export const receiveLoginErrors = (errors) => {
+  return {  type: RECEIVE_LOGIN_ERRORS, errors  };
+};
+
 export const loginUser = (user) => (dispatch) => {
   return SessionUtil.loginUser(user)
-    .then( (currentUser) => dispatch(receiveCurrentUser(currentUser))
+    .then( (currentUser) => dispatch(receiveCurrentUser(currentUser),
+    (errorRes) => dispatch(receiveLoginErrors(errorRes.responseJSON)))
   );
 };
 export const logoutUser = (user) => (dispatch) => {
--- frontend/actions/user_actions.js
+++ frontend/actions/user_actions.js
@@ -1,6 +1,7 @@
 import * as UsersUtil from '../utils/users_util';
 
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const RECEIVE_USER_ERRORS = 'RECEIVE_USER_ERRORS';
 export const RECEIVE_USERS = 'RECEIVE_USERS';
 
 export const receiveCurrentUser = (user) => {
@@ -8,6 +9,10 @@ export const receiveCurrentUser = (user) => {
     type: RECEIVE_CURRENT_USER,
     user: user
   };
+};
+
+export const receiveUserErrors = (errors) => {
+  return {  type: RECEIVE_USER_ERRORS, errors  };
 };
 
 export const receiveUsers = (users) => {
@@ -19,6 +24,7 @@ export const receiveUsers = (users) => {
 
 export const createUser = (user) => (dispatch) => {
   return UsersUtil.createUser(user)
-    .then( (currentUser) => dispatch(receiveCurrentUser(currentUser))
+    .then( (currentUser) => dispatch(receiveCurrentUser(currentUser)),
+    (errorRes) => dispatch(receiveUserErrors({server: errorRes.responseJSON}))
   );
 };
