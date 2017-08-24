2beebb12909ead83064d2c50c18d1632fec421b3
Omayemi Agbeyegebe <grend500@gmail.com>
2017-08-24 13:14:58 -0400

    create a default redirection

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import LandingContainer from './landing/landing_container';
 import { AuthRoute, ProtectedRoute } from '../utils/route_util';
+import { Switch } from 'react-router-dom';
 import Header from './header/header';
 
 const App = () => (
   <div>
-    <div id='header' />
     <Header />
     <AuthRoute path='/' component={LandingContainer} />
+    <Switch>
+      <ProtectedRoute path='/' render={() => ''}/>
+    </Switch>
   </div>
 );
 
