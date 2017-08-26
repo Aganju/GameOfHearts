4765834cf493a3014776a57f74106d6137b5327a
Omayemi Agbeyegebe <grend500@gmail.com>
2017-08-26 08:56:18 -0400

    add profile fetch and update ajax calls

--- frontend/utils/users_util.js
+++ frontend/utils/users_util.js
@@ -5,3 +5,18 @@ export const createUser = (user) => {
 	  data: { user: user }
   });
 };
+
+export const getProfile = (username) => {
+  return $.ajax({
+      url: `/api/users/${username}`,
+      method: 'GET',
+  });
+};
+
+export const updateProfile = (profile) => {
+  return $.ajax({
+      url: '/api/users/',
+      method: 'GET',
+      data: { user: profile },
+  });
+};
