62afbfde8e057d488f9428a6d630f92dea19c277
Omayemi Agbeyegebe <grend500@gmail.com>
2017-08-25 20:09:08 -0400

    add new routes and move login show to sessions

--- config/routes.rb
+++ config/routes.rb
@@ -56,7 +56,7 @@ Rails.application.routes.draw do
   root to: 'static_pages#root'
 
   namespace :api, defaults: { format: :json } do
-    resources :users, only: [:create]
+    resources :users, except: [:new, :edit]
     resource :session, only: [:create, :destroy]
   end
 end
