aaf75ae0a2796785b5897adf72e14875227e62b9
Omayemi Agbeyegebe <grend500@gmail.com>
2017-08-28 09:41:29 -0400

    add profile routes to app

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import LandingContainer from './landing/landing_container';
-import { AuthRoute, ProtectedRoute } from '../utils/route_util';
 import { Switch } from 'react-router-dom';
 import Header from './header/header';
+import { AuthRoute, ProtectedRoute } from '../utils/route_util';
+import LandingContainer from './landing/landing_container';
+import ProfileContainer from './profile/profile_container';
 
 const App = () => (
   <div>
     <Header />
     <AuthRoute path='/' component={LandingContainer} />
     <Switch>
+      <ProtectedRoute path='/profile/:username' component={ProfileContainer} />
+      <ProtectedRoute path='/profile' component={ ProfileContainer } />
       <ProtectedRoute path='/' render={() => ''}/>
     </Switch>
   </div>
