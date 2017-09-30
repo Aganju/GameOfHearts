1f40dec86b496f33268d8f87c7d8a24993aada4d
Omayemi Agbeyegebe <grend500@gmail.com>
2017-09-30 13:51:07 -0400

    add plugins for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,28 @@
 var path = require('path');
+var webpack = require('webpack');
+
+var prodPlugins  = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+var plugins =  process.env.NODE_ENV === 'production' ? prodPlugins : [];
 
 module.exports = {
+  context: __dirname,
   entry: './frontend/game_of_hearts.jsx',
   output: {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
     filename: 'bundle.js',
   },
+  plugins: plugins,
   module: {
     loaders: [
       {
