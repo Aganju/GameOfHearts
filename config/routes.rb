b6424ec58155807e5d6b559066bd546c3c81577f
Omayemi Agbeyegebe <grend500@gmail.com>
2018-01-08 10:13:13 -0500

    add conversations routes

--- config/routes.rb
+++ config/routes.rb
@@ -58,6 +58,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :users, except: [:new, :edit]
     resources :questions, except: [:new, :edit]
+    resources :conversations, except: [:new, :edit]
     resource :session, only: [:create, :destroy]
   end
 end
