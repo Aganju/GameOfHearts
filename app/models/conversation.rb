1276a9b84f0ff9097f9a353510f086e260c79785
Omayemi Agbeyegebe <grend500@gmail.com>
2018-01-08 10:12:57 -0500

    add validations for nonempty message and user include in conversation helper function

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,6 +1,12 @@
 class Conversation < ActiveRecord::Base
   validates :first_user, :second_user, presence: true
+  validates :last_message, presence: true, allow_blank: false
 
   belongs_to :first_user, class_name: :User
   belongs_to :second_user, class_name: :User
+  has_many :messages
+
+  def includes_user(user)
+    user.id == self.first_user_id || user.id == self.second_user_id
+  end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,6 @@
 class Message < ActiveRecord::Base
   validates :conversation, :user, presence: true
+  validates :body, presence: true, allow_blank: false
 
   belongs_to :conversation
   belongs_to :user
