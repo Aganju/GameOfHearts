0006068b37dde0c706ac5d62fab4cfa68b471830
Omayemi Agbeyegebe <grend500@gmail.com>
2018-01-17 14:02:56 -0500

    change the action dispatched by logout user

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -1,7 +1,12 @@
 import * as SessionUtil from '../utils/session_util';
 import { receiveCurrentUser } from './user_actions';
 
+export const LOGOUT_USER = 'LOGOUT_USER';
 export const RECEIVE_LOGIN_ERRORS = 'RECEIVE_LOGIN_ERRORS';
+
+export const logUserOut = () => {
+  return { type: LOGOUT_USER };
+};
 
 export const receiveLoginErrors = (errors) => {
   return {  type: RECEIVE_LOGIN_ERRORS, errors  };
@@ -15,6 +20,6 @@ export const loginUser = (user) => (dispatch) => {
 };
 export const logoutUser = (user) => (dispatch) => {
   return SessionUtil.logoutUser(user)
-    .then( (currentUser) => dispatch(receiveCurrentUser(currentUser))
+    .then( (currentUser) => dispatch(logUserOut())
   );
 };
