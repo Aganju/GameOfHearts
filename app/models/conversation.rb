31f777d99fb21f56b3a1e319073ccf1bcc49ad81
Omayemi Agbeyegebe <grend500@gmail.com>
2018-01-17 07:30:16 -0500

    validate last message only on update and add other user helper function

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,12 +1,18 @@
 class Conversation < ActiveRecord::Base
   validates :first_user, :second_user, presence: true
-  validates :last_message, presence: true, allow_blank: false
+  validates :last_message, presence: true, on: :update
 
   belongs_to :first_user, class_name: :User
   belongs_to :second_user, class_name: :User
+
   has_many :messages
+  belongs_to :last_message, class_name: :Message
 
   def includes_user(user)
     user.id == self.first_user_id || user.id == self.second_user_id
+  end
+
+  def other_user(user_id)
+    user_id == self.first_user_id ? self.second_user : self.first_user
   end
 end
