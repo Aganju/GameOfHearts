8cca4040e88d34bfdc9d014e562db216604338a6
Omayemi Agbeyegebe <grend500@gmail.com>
2018-01-17 14:03:47 -0500

    add conversations reducer and refactor root reducer to allow for state clearing on logout

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,12 +1,24 @@
 import { combineReducers } from 'redux';
+import ConversationsReducer from './conversations_reducer';
 import ErrorsReducer from './errors_reducer';
 import ProfileReducer from './profile_reducer';
 import QuestionsAndAnswersReducer from './questions_and_answers_reducer';
 import SessionReducer from './session_reducer';
 import UIReducer from './ui_reducer';
 import UsersReducer from './users_reducer';
+import { LOGOUT_USER } from '../actions/session_actions';
 
-const rootReducer = combineReducers({
+const rootReducer = (state, action) => {
+    if (action.type === LOGOUT_USER){
+      state = undefined;
+    }
+
+    return appReducer(state, action);
+
+};
+
+const appReducer = combineReducers({
+  conversations: ConversationsReducer,
   errors: ErrorsReducer,
   profile: ProfileReducer,
   questions_and_answers: QuestionsAndAnswersReducer,
